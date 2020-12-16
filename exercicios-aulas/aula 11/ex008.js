a1711db38d270ab4f56f1ea83e34290052543444
Wanderson-Melo <75035633+Wanderson-Melo@users.noreply.github.com>
2020-12-16 17:03:18 -0300

    Exercícios das aulas 11 e 12

---
+++ exercicios-aulas/aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 15;
+console.log(`A velocidade do seu carro é de ${vel}km/h`);
+if(vel>60){ //Condição Simples
+    console.log(`Você foi MULTADO!`)
+}
+console.log(`Dirija sempre utilizando cinto de seguranção!`);
---
+++ exercicios-aulas/aula 12/ex012.js
@@ -0,0 +1,6 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas`)
+if(hora >= 5 && hora < 12){console.log('Bom dia!')}
+else if(hora >= 12 && hora < 19){console.log('Boa tarde!')}
+else{console.log('Boa noite!')}
